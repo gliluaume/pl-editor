3484cdf3576baf16fb45bec96ab2c50c353c0333
guillaume <salicisguillaume@yahoo.fr>
2017-02-20 17:30:12 +0100

    configuration file

---
+++ configuration.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const configuration = {
+  playlists: {
+    'mon': 'mon.csv', 
+    'tue': 'tue.csv', 
+    'wed': 'wed.csv', 
+    'thu': 'thu.csv', 
+    'fri': 'fri.csv', 
+    'sat': 'sat.csv', 
+    'sun': 'sun.csv'
+  },
+
+  tagMapping: {
+    'Size': 'Image Size',
+    'Duration': 'Duration'
+  },
+
+  environment: {
+    videoExt: '.mp4'
+  }
+};
+
+
+module.exports = configuration;
